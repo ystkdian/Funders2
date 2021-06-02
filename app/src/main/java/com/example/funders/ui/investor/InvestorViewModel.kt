dba1ca0bee5b5a560c2e464475b7189d374d4e80
Yestika Dian Wulandari <ystkdian@gmail.com>
2021-06-02 16:58:26 +0700

    Menambahkan tambahan layout

--- app/src/main/java/com/example/funders/ui/investor/InvestorViewModel.kt
+++ app/src/main/java/com/example/funders/ui/investor/InvestorViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.funders.ui.ui.home
+package com.example.funders.ui.investor
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class InvestorViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
